7b1246c894ce43a95a5e6db56a8d9810ddab3bae
zerosouth <yn782@naver.com>
2021-04-25 13:01:47 +0900

    test fix

--- src/test/java/ShallWe/Refactoring/Testinitialize/TestDBInit.java
+++ src/test/java/ShallWe/Refactoring/Testinitialize/TestDBInit.java
@@ -34,9 +34,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(
-        properties = {"spring.config.location=classpath:application-init.yml"}
-)
+@SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Transactional
 @Rollback(false)
