a54882e4bbd6cfabe1c2365d31b02b0529e3d5b9
zerosouth <yn782@naver.com>
2021-04-26 20:59:29 +0900

    main /rds : 10.3 ,spring 10.3 settings

--- src/test/java/ShallWe/Refactoring/Testinitialize/TestDBInit.java
+++ src/test/java/ShallWe/Refactoring/Testinitialize/TestDBInit.java
@@ -35,10 +35,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(
-        properties = {"spring.config.location=classpath:application-init.yml"}
-)
-
+@SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestDBInit {
 
