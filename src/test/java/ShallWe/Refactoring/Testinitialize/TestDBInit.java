e49291cdb1966946d6ab8fbeaec93a35da349b2a
zerosouth <yn782@naver.com>
2021-04-27 01:41:24 +0900

    PORT TEST

--- src/test/java/ShallWe/Refactoring/Testinitialize/TestDBInit.java
+++ src/test/java/ShallWe/Refactoring/Testinitialize/TestDBInit.java
@@ -36,7 +36,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
-        properties = {"spring.config.location=classpath:application-init.yml"}
+        properties = {"spring.config.location=classpath:application-init.yml"},
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
 )
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 //@Transactional
