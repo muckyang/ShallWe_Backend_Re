424b7f4e9393502b7114b808d1ea78c76a4ce4de
zerosouth <yn782@naver.com>
2021-04-28 13:13:33 +0900

    yml fixed

--- src/test/java/ShallWe/Refactoring/EncryptTest.java
+++ src/test/java/ShallWe/Refactoring/EncryptTest.java
@@ -8,18 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class EncryptTest {
 
-    @Test
-    public void checkEncrypt(){
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        encryptor.setProvider(new BouncyCastleProvider());
-        encryptor.setPoolSize(2);
-        encryptor.setPassword("MySecretKey");
-        encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
-
-        String plainText = "root"; // 암호화 할 내용
-        String encryptedText = encryptor.encrypt(plainText);
-        String decryptedText = encryptor.decrypt(encryptedText);
-        System.out.println("Enc:"+encryptedText+", Dec:"+decryptedText);
-    }
+//    @Test
+//    public void checkEncrypt(){
+//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//        encryptor.setProvider(new BouncyCastleProvider());
+//        encryptor.setPoolSize(2);
+//        encryptor.setPassword("MySecretKey");
+//        encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
+//
+//        String plainText = "root"; // 암호화 할 내용
+//        String encryptedText = encryptor.encrypt(plainText);
+//        String decryptedText = encryptor.decrypt(encryptedText);
+//        System.out.println("Enc:"+encryptedText+", Dec:"+decryptedText);
+//    }
 
 }
