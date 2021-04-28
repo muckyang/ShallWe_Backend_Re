301b61603fa6d60f9440ee68836b2bd1e6686156
zerosouth <yn782@naver.com>
2021-04-28 13:08:36 +0900

    yml settings

--- src/test/java/ShallWe/Refactoring/EncryptTest.java
+++ src/test/java/ShallWe/Refactoring/EncryptTest.java
@@ -5,18 +5,18 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 public class EncryptTest {
 
-//    @Test
+    @Test
     public void checkEncrypt(){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         encryptor.setProvider(new BouncyCastleProvider());
         encryptor.setPoolSize(2);
-        encryptor.setPassword("xoxo");
+        encryptor.setPassword("MySecretKey");
         encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
 
-        String plainText = "암호화 할 문자";
+        String plainText = "root"; // 암호화 할 내용
         String encryptedText = encryptor.encrypt(plainText);
         String decryptedText = encryptor.decrypt(encryptedText);
         System.out.println("Enc:"+encryptedText+", Dec:"+decryptedText);
